2f49a560a7e56e46cc1f932cced245a739801d81
tolbarweb <tolbarweb@gmail.com>
2020-08-08 01:28:18 -0500

    Solo esponer un servicio api enfermedades

--- src/main/java/com/tolbargy/api/EnfermedadEstudianteRest.java
+++ src/main/java/com/tolbargy/api/EnfermedadEstudianteRest.java
@@ -3,12 +3,7 @@ package com.tolbargy.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,26 +20,6 @@ public class EnfermedadEstudianteRest {
 	@GetMapping("")
 	public List<EnfermedadEstudiante> listarTodos() {
 		return service.listarTodos();
-	}
-
-	@GetMapping("{id}")
-	public EnfermedadEstudiante listarPorId(@PathVariable int id) {
-		return service.listarPorId(id);
-	}
-
-	@PostMapping("")
-	public void registrar(@RequestBody EnfermedadEstudiante entidad) {
-		service.registrar(entidad);
-	}
-
-	@PutMapping("")
-	public void actualizar(@RequestBody EnfermedadEstudiante entidad) {
-		service.actualizar(entidad);
-	}
-
-	@DeleteMapping("{id}")
-	public void eliminar(@PathVariable int id) {
-		service.eliminar(id);
 	}
 
 }
