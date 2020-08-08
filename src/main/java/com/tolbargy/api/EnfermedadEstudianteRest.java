f2594ccb5cbc9f64f13bd06bdc954601a5e2145f
tolbarweb <tolbarweb@gmail.com>
2020-08-08 17:28:57 -0500

    api registrar enferemdad

--- src/main/java/com/tolbargy/api/EnfermedadEstudianteRest.java
+++ src/main/java/com/tolbargy/api/EnfermedadEstudianteRest.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,11 @@ public class EnfermedadEstudianteRest {
 	@GetMapping("")
 	public List<EnfermedadEstudiante> listarTodos() {
 		return service.listarTodos();
+	}
+
+	@PostMapping("")
+	public void registrar(@RequestBody EnfermedadEstudiante enfermedadEstudiante) {
+		service.registrar(enfermedadEstudiante);
 	}
 
 }
